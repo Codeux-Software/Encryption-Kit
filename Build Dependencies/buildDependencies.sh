56a81c6f1738412e86c47479c48979d0612b4c90
Michael Morris <michael@codeux.com>
2017-06-15 22:05:33 -0400

    Update dependencies

--- Build Dependencies/buildDependencies.sh
+++ Build Dependencies/buildDependencies.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export LIBRARY_LIBRESSL_VERSION="2.4.2";
-export LIBRARY_GPG_ERROR_VERSION="1.24"
-export LIBRARY_GCRYPT_VERSION="1.7.3"
+export LIBRARY_LIBRESSL_VERSION="2.5.4";
+export LIBRARY_GPG_ERROR_VERSION="1.27"
+export LIBRARY_GCRYPT_VERSION="1.7.7"
 export LIBRARY_OTR_VERSION="4.1.1"
 
 if [ "$1" == "build-libressl" ]; then
